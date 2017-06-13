1a45f8fa5362172aa9f905d96b5b6f9e899ee880
Zain Khalid <zak565@uowmail.edu.au>
2017-06-13 13:38:47 -0400

    initial commity

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Zain
+ *
+ * Created on June 13, 2017, 1:27 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
